c55920de070989858c50351fae212f4e34d8dec2
Aubrey <aubreyfinkelstein@gmail.com>
2023-02-22 15:01:52 -0700

    proof of concept work for having data in json files and import/mapping to components. some basic tailwind styling practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import techStack from './data/tech-stack-data.json';
+
 function App() {
+  const techProficient = techStack.proficient;
+  const techFamiliar = techStack.familiar;
+
   return (
-    <h1 className="test-class">Hello World</h1>
+    <div className='bg-gray-800 h-screen'>
+      <h1 className="test-class">Hello World</h1>
+      <div className='flex flex-wrap'>
+        <div className='flex flex-wrap w-1/2 h-1/2 mx-auto justify-center items-center gap-3'>
+          {techProficient.map(el =>
+            <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
+          )}
+        </div>
+        <div className='flex flex-wrap w-1/2 h-1/2 mx-auto justify-center items-center gap-3'>
+          {techFamiliar.map(el =>
+            <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
