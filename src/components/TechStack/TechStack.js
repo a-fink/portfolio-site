88cab1046d49647d18cc8954d8aaa02b07365e7d
Aubrey <aubreyfinkelstein@gmail.com>
2023-02-23 00:06:57 -0700

    minor fixes in TechStack component and file structure for NavBar

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -5,11 +5,11 @@ function TechStack(){
     const techFamiliar = techStack.familiar;
 
     return(
-        <div>
-            <h1>My Tech Stack</h1>
-            <div className='mx-3 flex flex-wrap gap-3 justify-center lg:flex-nowrap'>
+        <div className='w-full bg-gray-800 my-4 py-4'>
+            <h1 className='my-2 py-2'>My Tech Stack</h1>
+            <div className='flex flex-wrap gap-6 justify-center mx-3 lg:mx-8 lg:gap-8 lg:flex-nowrap xl:gap-24'>
                 <div>
-                    <h3>Proficient</h3>
+                    <h3 className='my-2 py-2'>Proficient</h3>
                     <div className='tech-stack-container'>
                     {techProficient.map(el =>
                         <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
@@ -17,7 +17,7 @@ function TechStack(){
                     </div>
                 </div>
                 <div>
-                <h3>Familiar</h3>
+                <h3 className='my-2 py-2'>Familiar</h3>
                     <div className='tech-stack-container'>
                     {techFamiliar.map(el =>
                         <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
