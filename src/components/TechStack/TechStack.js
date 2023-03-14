caca69838a97ca64b83b69e58dae4040f19e6ac8
Aubrey <aubreyfinkelstein@gmail.com>
2023-03-13 21:57:00 -0700

    added images, added text to tech stack and adjusted styling

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -5,14 +5,17 @@ function TechStack(){
     const techFamiliar = techStack.familiar;
 
     return(
-        <div className='w-full bg-gray-800 my-4 py-4'>
+        <div className='w-full min-h-screen bg-gray-800 my-4 py-4'>
             <h1 className='my-2 py-2'>My Tech Stack</h1>
-            <div className='flex flex-wrap gap-6 justify-center mx-3 lg:mx-8 lg:gap-8 lg:flex-nowrap xl:gap-24'>
+            <div className='flex flex-wrap gap-6 justify-center mx-3 lg:mx-8 lg:gap-8 xl:gap-24'>
                 <div>
                     <h3 className='my-2 py-2'>Proficient</h3>
                     <div className='tech-stack-container'>
                     {techProficient.map(el =>
-                        <img className='tech-stack-image' src={el.src} key={el.id} alt={el.name} title={el.name} aria-label={el.name} />
+                        <div className='tech-stack-div' key={el.id}>
+                            <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
+                            <p className='text-center text-white text-sm'>{el.name}</p>
+                        </div>
                     )}
                     </div>
                 </div>
@@ -20,7 +23,10 @@ function TechStack(){
                 <h3 className='my-2 py-2'>Familiar</h3>
                     <div className='tech-stack-container'>
                     {techFamiliar.map(el =>
-                        <img className='tech-stack-image' src={el.src} key={el.id} alt={el.name} title={el.name} aria-label={el.name} />
+                        <div className='tech-stack-div' key={el.id}>
+                        <img className='tech-stack-image' src={el.src} alt={el.name} title={el.name} aria-label={el.name} />
+                        <p className='text-center text-white'>{el.name}</p>
+                    </div>
                     )}
                     </div>
                 </div>
