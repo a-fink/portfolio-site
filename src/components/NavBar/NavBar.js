b2d5ba4e6b61b341e7e93194428b05758c0e88fb
Aubrey <aubreyfinkelstein@gmail.com>
2023-03-18 17:02:09 -0700

    Split contact buttons into their own component to dry up code. Adjusted styling on buttons, links, and headings to make them a bit larger. Converted email button to copy email to clipboard instead of being a mailto link.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,5 @@
-import emailLogo from '../../images/icons8-mail-120.png';
-import githubLogo from '../../images/icons8-github-120.png';
-import linkedinLogo from '../../images/icons8-linkedin-120.png';
-
+import ContactButtons
+ from "../ContactButtons/ContactButtons";
 function NavBar(){
     return (
         <nav className='w-full px-6 pt-2 pb-3 fixed z-20 top-0 flex flex-wrap justify-center xl:justify-between items-center gap-3 bg-darkgray'>
@@ -16,12 +14,7 @@ function NavBar(){
                     <li>|</li>
                     <li><a className='nav-link' href='#contact'>Contact</a></li>
                 </ul>
-                <ul className='flex items-center gap-2'>
-                    <li><a className='block graphic-link' href='https://www.linkedin.com/in/aubreyfinkelstein' target="_blank" rel="noreferrer"><img src={linkedinLogo} title='Connect via LinkedIn' alt='LinedIn' className='contact-link-image'/></a></li>
-                    <li><a className='block graphic-link' href='https://github.com/a-fink/' target="_blank" rel="noreferrer"><img src={githubLogo} title='View my GitHub' alt='GitHub' className='contact-link-image'/></a></li>
-                    <li><a className='block graphic-link' href='mailto:aubreyfinkelstein@gmail.com' target="_blank" rel="noreferrer"><img src={emailLogo} title='Contact Me' alt='Email' className='contact-link-image'/></a></li>
-                    <li><button className='px-2 py-0.25 graphic-link'><a className='block' href='https://drive.google.com/file/d/1tjtNuPgVn4a0JFhTU6MRE0m0xz3sb3j-/view?usp=share_link' target="_blank" rel="noreferrer">Resume</a></button></li>
-                </ul>
+                <ContactButtons />
             </div>
         </nav>
     );
